be23a83a4f9d4ba38feafe22a86e6fd2ae43d443
crupib <crupib@gmail.com>
2020-04-20 19:56:39 -0400

    add rust func

---
+++ projects/func/target/debug/deps/func-e4187b4137a61a51.d
@@ -0,0 +1,5 @@
+/Users/williamcrupi/Documents/github/rust/projects/func/target/debug/deps/func-e4187b4137a61a51: src/main.rs
+
+/Users/williamcrupi/Documents/github/rust/projects/func/target/debug/deps/func-e4187b4137a61a51.d: src/main.rs
+
+src/main.rs:
---
+++ projects/func/target/debug/func.d
@@ -0,0 +1,1 @@
+/Users/williamcrupi/Documents/github/rust/projects/func/target/debug/func: /Users/williamcrupi/Documents/github/rust/projects/func/src/main.rs
