80ce412d13d32b9e09c767b1583ec64f498f0f16
crupib <crupib@gmail.com>
2020-05-10 11:00:29 -0400

    rect/struc

---
+++ projects/structs/target/debug/deps/structs-d08aad3decd271cc.d
@@ -0,0 +1,5 @@
+/Users/williamcrupi/Documents/github/rust/projects/structs/target/debug/deps/structs-d08aad3decd271cc: src/main.rs
+
+/Users/williamcrupi/Documents/github/rust/projects/structs/target/debug/deps/structs-d08aad3decd271cc.d: src/main.rs
+
+src/main.rs:
---
+++ projects/structs/target/debug/structs.d
@@ -0,0 +1,1 @@
+/Users/williamcrupi/Documents/github/rust/projects/structs/target/debug/structs: /Users/williamcrupi/Documents/github/rust/projects/structs/src/main.rs
