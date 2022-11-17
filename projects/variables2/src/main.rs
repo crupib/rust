c68fa5950e9fde2c25b6e70c78030eed14271ba6
crupib <crupib@gmail.com>
2022-11-16 21:03:03 -0500

    updates

--- projects/variables2/src/main.rs
+++ projects/variables2/src/main.rs
@@ -1,8 +1,14 @@
-fn main() {
+fn main() { 
+    const MAX_POINTS: u32 = 100_000;
     let mut x = 5;
     let y = 10;
+    let z = 5;
+    let z = z + 1;
+    let z = z * 2;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
+    println!("Const is : {}", MAX_POINTS); 
+    println!("The final value of z is {}", z)
 }
