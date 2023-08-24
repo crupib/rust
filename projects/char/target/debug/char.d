84fcd2813ed7ad987a924213456294f34b0b1626
Bill <crupib@gmail.com>
2023-08-24 19:14:51 -0400

    rust update

--- projects/char/target/debug/char.d
+++
@@ -1,1 +0,0 @@
-C:\Users\crupi\source\rust\projects\char\target\debug\char.exe: C:\Users\crupi\source\rust\projects\char\src\main.rs
--- projects/char/target/debug/deps/char.d
+++
@@ -1,5 +0,0 @@
-C:\Users\crupi\source\rust\projects\char\target\debug\deps\char.exe: src\main.rs
-
-C:\Users\crupi\source\rust\projects\char\target\debug\deps\char.d: src\main.rs
-
-src\main.rs:
