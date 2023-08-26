4d729354177b71931c4a7663ff37b2022e616d62
Bill <crupib@gmail.com>
2023-08-26 11:25:16 -0400

    Rust updates

---
+++ projects/communicator/target/debug/deps/communicator.d
@@ -0,0 +1,5 @@
+C:\Users\crupi\source\rust\projects\communicator\target\debug\deps\communicator.exe: src\main.rs
+
+C:\Users\crupi\source\rust\projects\communicator\target\debug\deps\communicator.d: src\main.rs
+
+src\main.rs:
---
+++ projects/copu/target/debug/deps/copu.d
@@ -0,0 +1,5 @@
+C:\Users\crupi\source\rust\projects\copu\target\debug\deps\copu.exe: src\main.rs
+
+C:\Users\crupi\source\rust\projects\copu\target\debug\deps\copu.d: src\main.rs
+
+src\main.rs:
