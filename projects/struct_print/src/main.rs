ce54d097cefebe5ed2b8c0cd840c9940a46f37e9
Bill <crupib@gmail.com>
2023-12-01 11:54:08 -0500

    structs impl

--- projects/struct_print/src/main.rs
+++ projects/struct_print/src/main.rs
@@ -4,12 +4,27 @@ struct Rectangle {
 	height: u32,
 }
 impl Rectangle {
-    fn area (&self) -> u32 {
-    self.width * self.height
+    fn area(&self) -> u32 {
+        self.width * self.height
     }
 }
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
+}
+impl Rectangle {
+        fn can_hold(&self, other: &Rectangle) -> bool {
+            self.width > other.width && self.height > other.height
+        }
+}
 fn main() {
-    let rect1 = Rectangle {width:30, height:50};
-    println!("rect1 is {:#?}", rect1);
-    println!("area of the Rectangle is {}",rect1.area());
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("The three areas {}, {}, {} ", rect1.area(), rect2.area(), rect3.area());
+    let sq = Rectangle::square(4);
+    println!("Square Rectangle {:?}",sq);
 }
