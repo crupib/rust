ea7c9bd270d9498d8f8e36ed80ec344a1e804597
Bill <crupib@gmail.com>
2024-01-15 12:54:34 -0500

    enum stuff

--- projects/enum_test/src/main.rs
+++ projects/enum_test/src/main.rs
@@ -1,27 +1,20 @@
 
 #[derive(Debug)]
 
-enum IpAddrKind {
-    V4,
-    V6,
+enum IpAddr {
+    V4(u8,u8,u8,u8),
+    V6(String),
 }
-#[derive(Debug)]
-
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+enum Message {
+    Quit,
+    Move {x: i32, y:i32 },
+    Write(String),
+    ChangeColor (i32, i32, i32),
 }
 fn main() {
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
-    println!("Home {}",home.address);
-    println!("Home {:?}",home.kind);
-    println!("Loopback {}",loopback.address);
-    println!("Loopback {:?}",loopback.kind);
+    let home = IpAddr::V4(127,0,0,1);
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    println!("Home {:?}",home);
+    println!("Loopback {:?}",loopback);
 }
